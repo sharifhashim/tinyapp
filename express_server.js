daa983efc5c4d621bee0cd94da7e84ae7f1f419d
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-08 23:22:53 +0000

    require body-parser, add route to /urls/new, add post route to receive form submission, implement generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -17,6 +19,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>")
 });
@@ -31,6 +37,19 @@ app.get("/urls/:shortURL", (req, res) => {
   console.log(templateVars)
   res.render("urls_show", templateVars)
 })
+app.post("/urls", (req, res) => {
+  console.log(req.body); // 
+  res.send("Ok");
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = function() {
+  let alphaNumeric = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let randomString = ""
+  for (let i = 0; i < 6; i++) {
+    randomString += alphaNumeric[Math.floor(Math.random() * alphaNumeric.length)]
+  }
+  return randomString
+}

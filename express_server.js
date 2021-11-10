7238182f2326bde120f061c0c8045e24344a08df
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-10 01:07:04 +0000

    add route so that logout button clears cookies

--- express_server.js
+++ express_server.js
@@ -22,7 +22,8 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars)
 })
 
 app.get("/hello", (req, res) => {
@@ -30,12 +31,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"], 
+    urls: urlDatabase 
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  }
   console.log(templateVars)
   res.render("urls_show", templateVars)
 });
@@ -63,6 +71,11 @@ app.post("/urls/:shortURL", (req, res) => {
 
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username)
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
   res.redirect("/urls")
 });
 

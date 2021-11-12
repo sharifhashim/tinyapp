dde275a23fb3637c0739de7706f94a6d6050dd7d
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-12 02:43:34 +0000

    move helper functions to helpers.js and write tests

---
+++ helpers.js
@@ -0,0 +1,28 @@
+const getUserByEmail = function(email, database) {
+  for (key in database) {
+    if (database[key].email === email) {
+      return database[key]
+    }
+  }
+  return null;
+}
+
+const urlsForUser = function(id, database) {
+  let urlsForUserDB = {}
+  for (let key in database) {
+    if (database[key].userID === id) {
+      urlsForUserDB[key] = database[key]
+    }
+  }
+  return urlsForUserDB
+}
+
+const generateRandomString = function() {
+  let alphaNumeric = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let randomString = ""
+  for (let i = 0; i < 6; i++) {
+    randomString += alphaNumeric[Math.floor(Math.random() * alphaNumeric.length)]
+  }
+  return randomString
+}
+module.exports = { getUserByEmail, urlsForUser, generateRandomString}
